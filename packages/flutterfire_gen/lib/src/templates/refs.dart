0478aebcc8ddace49626fabc7fef20b4bf092814
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-06-14 22:50:58 +0900

    feat: add refs template

---
+++ packages/flutterfire_gen/lib/src/templates/refs.dart
@@ -0,0 +1,20 @@
+import '../flutterfire_gen.dart';
+
+// ignore: comment_references
+/// Returns [CollectionReference] and [DocumentReference] template.
+String refsTemplate({required FirestoreDocumentConfig config}) {
+  return """
+/// A [CollectionReference] to ${config.collectionName} collection to read.
+final read${config.baseClassName}CollectionReference =
+    FirebaseFirestore.instance.collection('${config.collectionName}').withConverter(
+          fromFirestore: (ds, _) => ${config.readClassName}.fromDocumentSnapshot(ds),
+          toFirestore: (obj, _) => obj.toJson(),
+        );
+
+/// A [DocumentReference] to ${config.documentName} document to read.
+DocumentReference<${config.readClassName}> read${config.baseClassName}DocumentReference({
+  required String ${config.documentName}Id,
+}) =>
+    read${config.baseClassName}CollectionReference.doc(${config.documentName}Id);
+""";
+}
