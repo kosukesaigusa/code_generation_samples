a6f0617452d7218c7338d71797e2d48a5ffc4b3c
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-08-12 14:32:09 +0900

    chore: update import sentence

--- packages/flutterfire_gen_annotation/lib/flutterfire_gen_annotation.dart
+++ packages/flutterfire_gen_annotation/lib/flutterfire_gen_annotation.dart
@@ -1,4 +1,4 @@
+export 'src/always_use_server_timestamp.dart';
 export 'src/default.dart';
 export 'src/field_value.dart';
 export 'src/firestore_document.dart';
-export 'src/server_timestamp.dart';
