b748d8369f01eeda38a0080894ab41ee9ad69e1f
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-10-22 13:00:15 +0900

    feat: update FirestoreDocument annotation fields

--- packages/flutterfire_gen_annotation/lib/src/firestore_document.dart
+++ packages/flutterfire_gen_annotation/lib/src/firestore_document.dart
@@ -1,18 +1,13 @@
-import 'package:meta/meta.dart';
-
 /// An annotation to define Cloud Firestore document.
 class FirestoreDocument {
   /// Creates a [FirestoreDocument].
   const FirestoreDocument({
-    @visibleForTesting this.useFakeFirebaseFirestore = false,
     required this.path,
     required this.documentName,
-    this.includeDocumentReferenceField = false,
-    this.generateCopyWith = false,
+    this.includePathField,
+    this.includeDocumentReferenceField,
+    this.generateCopyWith,
   });
-
-  /// Set true if you want to use FakeFirebaseFirestore for testing.
-  final bool useFakeFirebaseFirestore;
 
   /// A path to firestore document.
   ///
@@ -32,29 +27,12 @@ class FirestoreDocument {
   /// - userPostLike
   final String documentName;
 
+  /// Whether to include `path` field in ReadEntity class.
+  final bool? includePathField;
+
   /// Whether to include `DocumentReference` field in ReadEntity class.
-  final bool includeDocumentReferenceField;
+  final bool? includeDocumentReferenceField;
 
   /// Whether to generate `copyWith` method in ReadEntity class.
-  final bool generateCopyWith;
-
-  /// A [RegExp] source string to parse [useFakeFirebaseFirestore] from
-  /// annotation.
-  static const useFakeFirebaseFirestoreRegExpSource =
-      r'useFakeFirebaseFirestore:\s*(true|false)';
-
-  /// A [RegExp] source string to parse [path] from annotation.
-  static const pathRegExpSource = r"path:\s*'([^']*)'";
-
-  /// A [RegExp] source string to parse [documentName] from annotation.
-  static const documentNameRegExpSource = r"documentName:\s*'([^']*)'";
-
-  /// A [RegExp] source string to parse [includeDocumentReferenceField] from
-  /// annotation.
-  static const includeDocumentReferenceFieldRegExpSource =
-      r'includeDocumentReferenceField:\s*(true|false)';
-
-  /// A [RegExp] source string to parse [generateCopyWith] from annotation.
-  static const generateCopyWithRegExpSource =
-      r'generateCopyWith:\s*(true|false)';
+  final bool? generateCopyWith;
 }
