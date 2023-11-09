2a3002a3c572fc581df3818dc53974f8e1c650cb
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-11-10 02:22:48 +0900

    feat: add convertSnakeToCamel: true sample

---
+++ packages/from_json_generator/test/helper/repository_snake.dart
@@ -0,0 +1,70 @@
+// See: https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-a-repository
+
+import 'package:from_json_generator_annotation/from_json_generator_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'repository_snake.from_json.dart';
+
+@FromJson(convertSnakeToCamel: true)
+class Repository {
+  Repository({
+    required this.id,
+    required this.name,
+    required this.private,
+    required this.starGazersCount,
+    required this.topics,
+    required this.visibility,
+    required this.permissions,
+    required this.securityAndAnalysis,
+  });
+
+  factory Repository.fromJson(Map<String, dynamic> json) =>
+      _$RepositoryFromJson(json);
+
+  final String id;
+
+  final String name;
+
+  @Default(false)
+  final bool private;
+
+  final int starGazersCount;
+
+  @Default(<String>[])
+  List<String> topics;
+
+  @_visibilityConverter
+  final Visibility visibility;
+
+  final Map<String, bool> permissions;
+
+  final Map<String, Map<String, String>> securityAndAnalysis;
+}
+
+enum Visibility {
+  public,
+  private,
+  ;
+
+  factory Visibility.fromString(String visibilityString) {
+    switch (visibilityString) {
+      case 'public':
+        return Visibility.public;
+      case 'private':
+        return Visibility.private;
+    }
+    throw ArgumentError('visibility is not valid: $visibilityString');
+  }
+}
+
+const _visibilityConverter = _VisibilityConverter();
+
+class _VisibilityConverter implements JsonConverter<Visibility, String> {
+  const _VisibilityConverter();
+
+  @override
+  Visibility fromJson(String json) => Visibility.fromString(json);
+
+  @override
+  String toJson(Visibility visibility) => visibility.name;
+}
---
+++ packages/from_json_generator/test/helper/repository_snake.from_json.dart
@@ -0,0 +1,29 @@
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'repository_snake.dart';
+
+// **************************************************************************
+// FromJsonGenerator
+// **************************************************************************
+
+Repository _$RepositoryFromJson(Map<String, dynamic> json) => Repository(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      private: json['private'] as bool? ?? false,
+      starGazersCount: json['star_gazers_count'] as int,
+      topics: (json['topics'] as List<dynamic>?)
+              ?.map((e) => e as String)
+              .toList() ??
+          const <String>[],
+      visibility: _visibilityConverter.fromJson(json['visibility'] as String),
+      permissions: (json['permissions'] as Map<String, dynamic>)
+          .map((k, v) => MapEntry(k, v as bool)),
+      securityAndAnalysis:
+          (json['security_and_analysis'] as Map<String, dynamic>).map((k, v) =>
+              MapEntry(
+                  k,
+                  (v as Map<String, dynamic>)
+                      .map((k, v) => MapEntry(k, v as String)))),
+    );
